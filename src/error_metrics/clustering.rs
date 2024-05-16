49b6d76eb49401517ff3787e6b027eeb3be27348
Sam Vente <savente93@proton.me>
2024-05-16 18:09:30 +0200

    add simple binary accuracy test

--- src/error_metrics/clustering.rs
+++ src/error_metrics/clustering.rs
@@ -1,27 +1,30 @@
-pub fn adjusted_mutual_info_score(predictions: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn adjusted_rand_score(predictions: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn completeness_score(predictions: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn fowlkes_mallows_score(predictions: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn homogeneity_score(predictions: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn mutual_info_score(predictions: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn normalized_mutual_info_score(predictions: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn rand_score(predictions: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn v_measure_score(predictions: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
+// use polars::error::PolarsResult as Result;
+// use polars::prelude::*;
+
+// pub fn adjusted_mutual_info_score(predictions: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn adjusted_rand_score(predictions: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn completeness_score(predictions: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn fowlkes_mallows_score(predictions: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn homogeneity_score(predictions: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn mutual_info_score(predictions: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn normalized_mutual_info_score(predictions: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn rand_score(predictions: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn v_measure_score(predictions: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
--- src/error_metrics/regression.rs
+++ src/error_metrics/regression.rs
@@ -1,64 +1,64 @@
-use polars::error::PolarsResult as Result;
-use polars::prelude::*;
+// use polars::error::PolarsResult as Result;
+// use polars::prelude::*;
 
-pub fn rsme(prediction: Series, truth: Series) -> Result<f64> {
-    let df = prediction
-        .with_name("prediction")
-        .into_frame()
-        .with_column(truth.with_name("truth"))?;
-    let err = df
-        .select([(col("prediction") - col("truth")).alias("err")])?
-        .select([(col("err") * col("err")).sqrt().alias("err_sqrt")])?;
-    df([col("err_sqrt").mean()])?.get(0)?.first()
-}
+// // pub fn rsme(prediction: Series, truth: Series) -> Result<f64> {
+// //     let df = prediction
+// //         .with_name("prediction")
+// //         .into_frame()
+// //         .with_column(truth.with_name("truth"))?;
+// //     let err = df
+// //         .select(&[(col("prediction") - col("truth")).alias("err")])?
+// //         .select(&[(col("err") * col("err")).sqrt().alias("err_sqrt")])?;
+// //     df([col("err_sqrt").mean()])?.get(0)?.first()
+// // }
 
-pub fn mae(prediction: Series, truth: Series) -> Result<f64> {
-    todo!()
-}
+// pub fn mae(prediction: Series, truth: Series) -> Result<f64> {
+//     todo!()
+// }
 
-// TODO implement
-pub fn explained_variance(predition: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn max_error(predition: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn mean_absolute_error(predition: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn mean_squared_error(predition: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn root_mean_squared_error(predition: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn mean_squared_log_error(predition: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn root_mean_squared_log_error(predition: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn median_absolute_error(predition: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn r2(predition: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn mean_poisson_deviance(predition: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn mean_gamma_deviance(predition: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn mean_absolute_percentage_error(predition: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn d2_absolute_error_score(predition: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn d2_pinball_score(predition: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
-pub fn d2_tweedie_score(predition: Series, truth: Series) -> Result<Series> {
-    todo!()
-}
+// // TODO implement
+// pub fn explained_variance(predition: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn max_error(predition: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn mean_absolute_error(predition: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn mean_squared_error(predition: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn root_mean_squared_error(predition: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn mean_squared_log_error(predition: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn root_mean_squared_log_error(predition: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn median_absolute_error(predition: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn r2(predition: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn mean_poisson_deviance(predition: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn mean_gamma_deviance(predition: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn mean_absolute_percentage_error(predition: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn d2_absolute_error_score(predition: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn d2_pinball_score(predition: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
+// pub fn d2_tweedie_score(predition: Series, truth: Series) -> Result<Series> {
+//     todo!()
+// }
