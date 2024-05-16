5a4d22d1e2912820f240e1c83457fe44552dc9c9
Sam Vente <savente93@proton.me>
2024-05-16 21:35:30 +0200

    fix recall, precision and f1 metrics and their tests

--- src/utils/testing.rs
+++ src/utils/testing.rs
@@ -1,22 +1,38 @@
+use polars::frame::DataFrame;
+use polars::prelude::*;
+
 #[macro_export]
 macro_rules! assert_eq_fl {
     ($left:expr, $right:expr) => {
-        let d = ($left as f64 - $right as f64).abs();
+        let ls = stringify!($left);
+        let l = $left;
+        let rs = stringify!($right);
+        let r = $right;
+
+        let d = (l as f64 - r as f64).abs();
         assert!(
             d < 0.00001,
-            "Expressions {} and {} differ by {}",
-            $left,
-            $right,
+            "Expressions {}={} and {}={} differ by {}",
+            ls,
+            l,
+            rs,
+            r,
             d
         );
     };
     ($left:expr, $right:expr, $tol:expr) => {
         let d = ($left as f64 - $right as f64).abs();
+        let ls = stringify!($left);
+        let l = $left;
+        let rs = stringify!($right);
+        let r = $right;
         assert!(
             d < $tol,
-            "Expressions {} and {} differ by {}",
-            $left,
-            $right,
+            "Expressions {}={} and {}={} differ by {}",
+            ls,
+            l,
+            rs,
+            r,
             d
         );
     };
@@ -48,4 +64,21 @@ macro_rules! assert_sr_close {
             diffs.filter(&uneq)
         );
     };
+}
+
+// courtesy of scikit-learn testing suite
+// made with an svm, we don't do predictions here,
+//so just include the result as it's fairly small
+pub fn iris_predictions() -> DataFrame {
+    df!(
+    "prediction" => [ 2,2,1,2,2,2,0,1,2,2,2,2,0,2,2,0,2,2,2,2,1,2,2,2,0,2,0,2,1,1,2,0,2,1,0,0,0,0,2,0,2,1,0,2,0,0,0,0,1,2,2,2,0,2,2,2,0,0,2,1,2,0,2,2,0,2,2,2,2,0,2,0,2,2,2 ],
+    "truth" => [ 2,2,0,2,1,1,0,1,2,1,2,1,1,1,1,0,2,2,1,0,2,1,2,2,0,1,0,2,1,0,1,0,1,1,0,0,0,0,2,0,1,2,0,1,0,1,1,0,0,1,1,1,1,2,1,1,0,0,2,0,1,0,2,2,0,1,1,1,1,0,2,0,1,2,2 ],
+    ).unwrap()
+}
+
+pub fn iris_predictions_binary() -> DataFrame {
+    df!(
+    "prediction" => [ 0,0,1,1,0,0,1,1,0,0,1,1,0,1,0,0,1,0,1,0,0,1,0,0,1,0,1,1,0,0,0,0,0,0,1,0,0,0,1,1,0,1,0,1,0,0,1,1,0,0 ],
+    "truth" =>      [ 0,0,1,1,1,0,1,0,1,0,1,0,1,1,0,0,1,0,1,0,0,1,1,0,1,0,1,0,0,0,0,0,0,1,1,0,0,1,1,1,1,1,0,1,0,0,1,1,1,0 ],
+    ).unwrap()
 }
