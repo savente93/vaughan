568d78119240f0df5ab3946eb0860613679b6a30
Sam Vente <savente93@proton.me>
2024-05-18 22:10:47 +0200

    add test for rsme and mape

--- src/error_metrics/regression.rs
+++ src/error_metrics/regression.rs
@@ -44,7 +44,7 @@ pub fn mean_absolute_percentage_error(
     truth_column: &str,
 ) -> Result<f64> {
     let d = data
-        .select(&[when(col(truth_column).eq(lit(0)))
+        .select(&[when(col(truth_column).eq(lit(0.0)))
             .then(col(prediction_column))
             .otherwise(col("_err") / col(truth_column))
             .abs()
@@ -109,6 +109,19 @@ mod test {
     }
 
     #[test]
+    fn test_root_mean_squared_error() -> Result<()> {
+        assert_eq_fl!(
+            root_mean_squared_error(compute_error(
+                get_skl_test_predictions().lazy(),
+                "truth",
+                "pred1"
+            ))?,
+            1.0
+        );
+        Ok(())
+    }
+
+    #[test]
     fn test_skl_mean_absolute_error() -> Result<()> {
         assert_eq_fl!(
             mean_absolute_error(compute_error(
@@ -117,6 +130,19 @@ mod test {
                 "pred1"
             ))?,
             1.0
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_skl_mean_absolute_percentage_error() -> Result<()> {
+        assert_eq_fl!(
+            mean_absolute_percentage_error(
+                compute_error(get_skl_test_predictions().lazy(), "truth", "pred1"),
+                "pred1",
+                "truth"
+            )?,
+            0.02
         );
         Ok(())
     }
