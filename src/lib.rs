3230a38b29788bad620fe26ffa1305a2e478b30d
Sam Vente <savente93@proton.me>
2024-05-19 15:40:07 +0200

    add some crate level lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+#![warn(missing_docs)]
+#![warn(absolute_paths_not_starting_with_crate)]
+#![warn(missing_debug_implementations)]
+#![warn(non_local_definitions)]
+#![warn(rust_2021_incompatible_or_patterns)]
+#![warn(single_use_lifetimes)]
+#![warn(unused_lifetimes)]
+#![warn(unreachable_pub)]
+#![warn(unused_results)]
+
 pub mod distance;
 pub mod error_metrics;
 pub mod information;
